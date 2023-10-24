6ef9287233885f0b6759d7323850c1a2af1959f9
Levi Powis <lpowis97@gmail.com>
2023-10-24 19:47:01 -0400

    adds seeded data into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Storm Front", price: 18.99, image_url: "https://upload.wikimedia.org/wikipedia/en/2/2b/Storm_Front.png", description: "The first book in the Dresden Files series by Jim Butcher.")
+
+Product.create(name: "The Stand", price: 28.99, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/The_Stand_%281978%29_front_cover%2C_first_edition.png/440px-The_Stand_%281978%29_front_cover%2C_first_edition.png", description: "Classic end of the world novel by Stephen King.")
+
+Product.create(name: "Changes", price: 18.99, image_url: "https://m.media-amazon.com/images/I/41Wy6us8heL._SY445_SX342_.jpg", description: "The 12th book in the Dresden Files series by Jim Butcher.")
