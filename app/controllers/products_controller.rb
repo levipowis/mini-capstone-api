1e3b6c6ba203b31f9efe6efd96875bb6e9b7b501
Levi Powis <lpowis97@gmail.com>
2023-10-24 20:02:36 -0400

    adds routes and methods to one_product and another_product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def get_one_product
+    product = Product.find(1)
+    render json: product.as_json
+  end
+
+  def get_another_product
+    product = Product.second
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "get_all_products"
+  get "/one_product", controller: "products", action: "get_one_product"
+  get "another_product", controller: "products", action: "get_another_product"
 end
