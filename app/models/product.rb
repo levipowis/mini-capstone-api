0aedc6a18a7c36f0259fa159a55885d2634b966c
Levi Powis <lpowis97@gmail.com>
2023-10-30 21:32:12 -0400

    adds validation for image_url to require .jpg, .gif, .png, or .jpeg - also updates test fixtures products.yml to include correct file extention for image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :price, presence: true, numericality: { only_numeric: true, greater_than: 0 }
   validates :description, length: { in: 10..500 }
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i,
+                                  :message => "must have a valid file type", multiline: true
 
   def is_discounted?
     if price <= 10
