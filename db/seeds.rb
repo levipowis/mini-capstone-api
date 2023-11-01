30e36a9be5f6863998fb61e48968b6d5b24455d2
Levi Powis <lpowis97@gmail.com>
2023-10-31 20:20:48 -0400

    seeds supplier data and adds supplier_id column to products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
-Product.create(name: "Storm Front", price: 18.99, image_url: "https://upload.wikimedia.org/wikipedia/en/2/2b/Storm_Front.png", description: "The first book in the Dresden Files series by Jim Butcher.")
+# Product.create(name: "Storm Front", price: 18.99, image_url: "https://upload.wikimedia.org/wikipedia/en/2/2b/Storm_Front.png", description: "The first book in the Dresden Files series by Jim Butcher.")
 
-Product.create(name: "The Stand", price: 28.99, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/The_Stand_%281978%29_front_cover%2C_first_edition.png/440px-The_Stand_%281978%29_front_cover%2C_first_edition.png", description: "Classic end of the world novel by Stephen King.")
+# Product.create(name: "The Stand", price: 28.99, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/The_Stand_%281978%29_front_cover%2C_first_edition.png/440px-The_Stand_%281978%29_front_cover%2C_first_edition.png", description: "Classic end of the world novel by Stephen King.")
 
-Product.create(name: "Changes", price: 18.99, image_url: "https://m.media-amazon.com/images/I/41Wy6us8heL._SY445_SX342_.jpg", description: "The 12th book in the Dresden Files series by Jim Butcher.")
+# Product.create(name: "Changes", price: 18.99, image_url: "https://m.media-amazon.com/images/I/41Wy6us8heL._SY445_SX342_.jpg", description: "The 12th book in the Dresden Files series by Jim Butcher.")
+
+Supplier.create(name: "Roc Books", email: "roc_books@test.com", phone_number: "888-555-4222")
+
+Supplier.create(name: "Houghton Mifflin", email: "hm@test.com", phone_number: "888-555-4234")
+
+Supplier.create(name: "Doubleday", email: "doubleday@test.com", phone_number: "888-555-1233")
