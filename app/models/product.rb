cb80593c1cc08dd08306544b91af6e5a5c7146c3
Levi Powis <lpowis97@gmail.com>
2023-10-31 22:11:58 -0400

    removes image_url from products table and removes all references to image_url from products controller, jbuilder template, and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :price, presence: true, numericality: { only_numeric: true, greater_than: 0 }
   validates :description, length: { in: 10..500 }
-  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i,
-                                  :message => "must have a valid file type", multiline: true
 
   belongs_to :supplier
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,12 +14,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "description", "image_url", "is_discounted?", "tax", "total", "inventory", "supplier_id", "supplier"], data.keys
+    assert_equal ["id", "name", "price", "description", "is_discounted?", "tax", "total", "inventory", "supplier_id", "supplier"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test", price: 10, image_url: "test.jpg", description: "test description", supplier_id: Supplier.first.id }
+      post "/products.json", params: { name: "test", price: 10, description: "test description", supplier_id: Supplier.first.id }
       assert_response 200
     end
 
@@ -37,7 +37,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
     assert_equal product.price.to_s, data["price"]
-    assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
     assert_equal product.supplier_id, data["supplier_id"]
 
