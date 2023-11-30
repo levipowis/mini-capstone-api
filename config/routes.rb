935d88966695df54cd1611f3c447da409baf01e6
Levi Powis <lpowis97@gmail.com>
2023-11-29 21:44:43 -0500

    adds destroy route for carted_products

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,29 @@
 Rails.application.routes.draw do
-  # index route/action
+  # products
   get "/products" => "products#index"
-  # show route/action
   get "/products/:id" => "products#show"
-  # create route/action
   post "/products" => "products#create"
-  # update route/action
   patch "/products/:id" => "products#update"
-  # destroy route/action
   delete "/products/:id" => "products#destroy"
 
+  # suppliers
   get "/suppliers" => "suppliers#index"
-
   get "/suppliers/:id" => "suppliers#show"
-
   post "/suppliers" => "suppliers#create"
-
   patch "/suppliers/:id" => "suppliers#update"
-
   delete "/suppliers/:id" => "suppliers#destroy"
 
+  # users
   post "/users" => "users#create"
 
+  # sessions
   post "/sessions" => "sessions#create"
-
   post "/orders" => "orders#create"
-
   get "/orders/:id" => "orders#show"
-
   get "/orders" => "orders#index"
 
-  # carted_products routes
+  # carted_products
   get "/carted_products" => "carted_products#index"
   post "/carted_products" => "carted_products#create"
+  delete "/carted_products/:id" => "carted_products#delete"
 end
