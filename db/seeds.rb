3b0c9f72eed82ce904ca14b21e0366bd577903ae
Levi Powis <lpowis97@gmail.com>
2023-11-30 19:59:55 -0500

    seeded images table

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,10 @@
 #   {name: "The Stand", price: "10.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/The_Stand_%281978%29_front_cover%2C_first_edition.png/440px-The_Stand_%281978%29_front_cover%2C_first_edition.png", description: "Classic end of the world novel by Stephen King.", inventory: nil, supplier_id: 3}
 # ])
 
-Image.create!(url: "https://m.media-amazon.com/images/I/41Wy6us8heL._SY445_SX342_.jpg", product_id: 4)
-Image.create!(url: "https://m.media-amazon.com/images/I/91tl+39MjEL._SX679_.jpg", product_id: 4)
+# Image.create!(url: "https://m.media-amazon.com/images/I/41Wy6us8heL._SY445_SX342_.jpg", product_id: 4)
+# Image.create!(url: "https://m.media-amazon.com/images/I/91tl+39MjEL._SX679_.jpg", product_id: 4)
+
+Image.create!(url: "https://m.media-amazon.com/images/I/51vRFvKZMXL._SY445_SX342_.jpg", product_id: 6)
+Image.create!(url: "https://m.media-amazon.com/images/I/51uVoC-gAZL._SY466_.jpg", product_id: 8)
+Image.create!(url: "https://m.media-amazon.com/images/I/41r0b4e7RfL._SY445_SX342_.jpg", product_id: 10)
+Image.create!(url: "https://m.media-amazon.com/images/I/8195LaJUCSL._SY466_.jpg", product_id: 11)
